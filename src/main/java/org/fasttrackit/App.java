c46b1439c6445cad068336ff80172479ab8af99d
Cimpoeru Diana <diana.cimpoeru@yahoo.com>
2023-02-07 00:09:22 +0200

    Initial commit (project structure)

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,14 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ *
+ */
+//one-line comment
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
